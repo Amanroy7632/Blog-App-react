fdd616849f3c958c37db32cf4fe64ba61a53cfe4
Aman Roy <yadavaman7632@gmail.com>
2024-02-25 11:33:03 +0530

    started with basics

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  return (
+    <>
+       
+      <h1 className=' text-blue-700 p-4 bg-green-300'>New Project Blog App with Appwrite</h1>
+     
+    </>
+  )
+}
+
+export default App
